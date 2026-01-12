b5afe4771f73eacacba338d01a3d1cd0d62d6f13
duck. <me@duck.me.uk>
2026-01-12 14:54:19 +0000

    Initialise app with App Factory model

--- slurp/__init__.py
+++ slurp/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, render_template, stream_template
+from flask import Flask, request, render_template, stream_template, Blueprint, current_app
 from flask_wtf import FlaskForm
 import tomllib
 
@@ -11,26 +11,6 @@ from slurp.fetchers.types import Format
 from slurp.fetchers import determine_fetcher, fetchers, YTDLPFetcher, CobaltFetcher
 from slurp.helpers import format_duration
 
-app = Flask(__name__)
-# app.config.from_prefixed_env(prefix='YDP')
-app.config.from_file(os.path.join(os.getcwd(), "config.toml"), load=tomllib.load, text=False)
-app.config['OUTPUTS'] = app.config.get('OUTPUTS', '').split(os.pathsep)
-
-# Fill fetchers config with configured fetchers.
-if app.config.get('FETCHER_YTDLP_ENABLED') is True:
-    app.logger.info('YTDLP fetcher enabled')
-    fetchers.append(YTDLPFetcher())
-
-if app.config.get('FETCHER_COBALT_ENABLED') is True:
-    app.logger.info('Cobalt fetcher enabled')
-    fetchers.append(CobaltFetcher(app.config.get('FETCHER_COBALT_URL', 'http://localhost:9000'), app.config.get('FETCHER_COBALT_KEY', None)))
-
-if len(fetchers) == 0:
-    # No fetchers configured - fatal error.
-    raise Exception('No fetchers enabled')
-
-app.jinja_env.filters['duration'] = format_duration
-
 
 class DownloadForm(FlaskForm):
     url = URLField('url', validators=[DataRequired(), URL()])
@@ -39,17 +19,18 @@ class DownloadForm(FlaskForm):
                          validators=[DataRequired(), AnyOf([v.name for v in Format])])
     directory = SelectField('directory', validators=[DataRequired()])
 
-@app.get('/')
+main_blueprint = Blueprint('main', __name__, template_folder='templates')
+@main_blueprint.get('/')
 def index():
     form = DownloadForm(request.args)
-    form.directory.choices = app.config['OUTPUTS']
+    form.directory.choices = current_app.config['OUTPUTS']
 
     return render_template('index.html', form=form, fetchers=fetchers)
 
-@app.post('/download')
+@main_blueprint.post('/download')
 def download():
     form = DownloadForm()
-    form.directory.choices = app.config['OUTPUTS']
+    form.directory.choices = current_app.config['OUTPUTS']
     if not form.validate_on_submit():
         return form.errors
 
@@ -65,5 +46,31 @@ def download():
         output=fetcher.get_media(form.url.data, Format[form.format.data], form.directory.data, form.slug.data)
     )
 
+def create_app(config_filename: str="config.toml") -> Flask:
+    app = Flask(__name__)
+    # app.config.from_prefixed_env(prefix='YDP')
+    app.config.from_file(os.path.join(os.getcwd(), config_filename), load=tomllib.load, text=False)
+    app.config['OUTPUTS'] = app.config.get('OUTPUTS', '').split(os.pathsep)
+
+    # Fill fetchers config with configured fetchers.
+    if app.config.get('FETCHER_YTDLP_ENABLED') is True:
+        app.logger.info('YTDLP fetcher enabled')
+        fetchers.append(YTDLPFetcher())
+
+    if app.config.get('FETCHER_COBALT_ENABLED') is True:
+        app.logger.info('Cobalt fetcher enabled')
+        fetchers.append(CobaltFetcher(app.config.get('FETCHER_COBALT_URL', 'http://localhost:9000'),
+                                      app.config.get('FETCHER_COBALT_KEY', None)))
+
+    if len(fetchers) == 0:
+        # No fetchers configured - fatal error.
+        raise Exception('No fetchers enabled')
+
+    app.jinja_env.filters['duration'] = format_duration
+
+    app.register_blueprint(main_blueprint)
+
+    return app
+
 def serve():
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    create_app().run(host='0.0.0.0', port=8000, debug=False)
