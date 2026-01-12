e3f5f481ef265303d3a81b38fb1ca28ae96964c6
duck. <me@duck.me.uk>
2026-01-12 20:45:52 +0000

    Lint using the Ruff linter
    Adds Github CI action for PRs to check linting complies with Ruff.

--- slurp/__init__.py
+++ slurp/__init__.py
@@ -1,36 +1,49 @@
 import os
-
-from flask import Flask, request, render_template, stream_template, Blueprint, current_app
-from flask_wtf import FlaskForm
 import tomllib
 
-from wtforms import URLField, StringField, SelectField
-from wtforms.validators import URL, DataRequired, AnyOf
+from flask import (
+    Blueprint,
+    Flask,
+    current_app,
+    render_template,
+    request,
+    stream_template,
+)
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField, URLField
+from wtforms.validators import URL, AnyOf, DataRequired
 
+from slurp.fetchers import CobaltFetcher, YTDLPFetcher, determine_fetcher, fetchers
 from slurp.fetchers.types import Format
-from slurp.fetchers import determine_fetcher, fetchers, YTDLPFetcher, CobaltFetcher
 from slurp.helpers import format_duration
 
 
 class DownloadForm(FlaskForm):
-    url = URLField('url', validators=[DataRequired(), URL()])
-    slug = StringField('slug', validators=[DataRequired()])
-    format = SelectField('format', choices=[(v.name, v.value) for v in Format],
-                         validators=[DataRequired(), AnyOf([v.name for v in Format])])
-    directory = SelectField('directory', validators=[DataRequired()])
+    url = URLField("url", validators=[DataRequired(), URL()])
+    slug = StringField("slug", validators=[DataRequired()])
+    format = SelectField(
+        "format",
+        choices=[(v.name, v.value) for v in Format],
+        validators=[DataRequired(), AnyOf([v.name for v in Format])],
+    )
+    directory = SelectField("directory", validators=[DataRequired()])
 
-main_blueprint = Blueprint('main', __name__, template_folder='templates')
-@main_blueprint.get('/')
+
+main_blueprint = Blueprint("main", __name__, template_folder="templates")
+
+
+@main_blueprint.get("/")
 def index():
     form = DownloadForm(request.args)
-    form.directory.choices = current_app.config['OUTPUTS']
+    form.directory.choices = current_app.config["OUTPUTS"]
 
-    return render_template('index.html', form=form, fetchers=fetchers)
+    return render_template("index.html", form=form, fetchers=fetchers)
 
-@main_blueprint.post('/download')
+
+@main_blueprint.post("/download")
 def download():
     form = DownloadForm()
-    form.directory.choices = current_app.config['OUTPUTS']
+    form.directory.choices = current_app.config["OUTPUTS"]
     if not form.validate_on_submit():
         return form.errors
 
@@ -40,37 +53,47 @@ def download():
         return str(e), 400
 
     return stream_template(
-        'download.html',
+        "download.html",
         fetcher=fetcher,
         metadata=fetcher.get_metadata(form.url.data, Format[form.format.data]),
-        output=fetcher.get_media(form.url.data, Format[form.format.data], form.directory.data, form.slug.data)
+        output=fetcher.get_media(
+            form.url.data, Format[form.format.data], form.directory.data, form.slug.data
+        ),
     )
 
-def create_app(config_filename: str="config.toml") -> Flask:
+
+def create_app(config_filename: str = "config.toml") -> Flask:
     app = Flask(__name__)
     # app.config.from_prefixed_env(prefix='YDP')
-    app.config.from_file(os.path.join(os.getcwd(), config_filename), load=tomllib.load, text=False)
-    app.config['OUTPUTS'] = app.config.get('OUTPUTS', '').split(os.pathsep)
+    app.config.from_file(
+        os.path.join(os.getcwd(), config_filename), load=tomllib.load, text=False
+    )
+    app.config["OUTPUTS"] = app.config.get("OUTPUTS", "").split(os.pathsep)
 
     # Fill fetchers config with configured fetchers.
-    if app.config.get('FETCHER_YTDLP_ENABLED') is True:
-        app.logger.info('YTDLP fetcher enabled')
+    if app.config.get("FETCHER_YTDLP_ENABLED") is True:
+        app.logger.info("YTDLP fetcher enabled")
         fetchers.append(YTDLPFetcher())
 
-    if app.config.get('FETCHER_COBALT_ENABLED') is True:
-        app.logger.info('Cobalt fetcher enabled')
-        fetchers.append(CobaltFetcher(app.config.get('FETCHER_COBALT_URL', 'http://localhost:9000'),
-                                      app.config.get('FETCHER_COBALT_KEY', None)))
+    if app.config.get("FETCHER_COBALT_ENABLED") is True:
+        app.logger.info("Cobalt fetcher enabled")
+        fetchers.append(
+            CobaltFetcher(
+                app.config.get("FETCHER_COBALT_URL", "http://localhost:9000"),
+                app.config.get("FETCHER_COBALT_KEY", None),
+            )
+        )
 
     if len(fetchers) == 0:
         # No fetchers configured - fatal error.
-        raise Exception('No fetchers enabled')
+        raise Exception("No fetchers enabled")
 
-    app.jinja_env.filters['duration'] = format_duration
+    app.jinja_env.filters["duration"] = format_duration
 
     app.register_blueprint(main_blueprint)
 
     return app
 
+
 def serve():
-    create_app().run(host='0.0.0.0', port=8000, debug=False)
+    create_app().run(host="0.0.0.0", port=8000, debug=False)
--- slurp/fetchers/ytdlp.py
+++ slurp/fetchers/ytdlp.py
@@ -6,25 +6,26 @@ from typing import Generator
 from markupsafe import escape
 from yt_dlp import YoutubeDL
 
-from slurp.fetchers.types import Format, Fetcher, MediaMetadata
+from slurp.fetchers.types import Fetcher, Format, MediaMetadata
 
 
 class YTDLPFetcher(Fetcher):
-    """ YTDLPFetcher is a fetcher that uses the YT-DLP library to download media exclusively from YouTube."""
+    """YTDLPFetcher is a fetcher that uses the YT-DLP library to download media exclusively from YouTube."""
+
     name = "yt-dlp"
 
-    service_names = ['Youtube']
-    service_urls = ['youtube.com', 'youtu.be']
+    service_names = ["Youtube"]
+    service_urls = ["youtube.com", "youtu.be"]
 
     class _Queuelogger:
-        """ queueLogger provides a yt-dlp compatible logging interface that emits exclusively to a queue."""
+        """queueLogger provides a yt-dlp compatible logging interface that emits exclusively to a queue."""
 
         def __init__(self, q: queue.Queue):
             self.q = q
 
         def debug(self, msg):
             # As recommended by library documentation
-            if msg.startswith('[debug] '):
+            if msg.startswith("[debug] "):
                 self.q.put(("debug", msg))
             else:
                 self.info(msg)
@@ -40,63 +41,74 @@ class YTDLPFetcher(Fetcher):
 
     @classmethod
     def _format_config(cls, fmt: Format) -> dict:
-        """ _format_config returns YT-DLP configuration to be used when downloading media in the given format.
+        """_format_config returns YT-DLP configuration to be used when downloading media in the given format.
         :param fmt: The desired media format.
         :return: A YT-DLP configuration parameters dictionary.
         """
         match fmt:
             case fmt.VIDEO_AUDIO:
                 return {
-                    'format': 'bestvideo*+bestaudio/best',
+                    "format": "bestvideo*+bestaudio/best",
                 }
                 # force codec to h264 m4a/mp4
                 # fails if this format isn't available, fix later
                 # return ['-f', 'bv*[vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]/b']
             case fmt.AUDIO_ONLY:
                 return {
-                    'format': 'm4a/bestaudio/best',
-                    'postprocessors': [{  # Extract audio using ffmpeg
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': 'm4a',
-                    }]
+                    "format": "m4a/bestaudio/best",
+                    "postprocessors": [
+                        {  # Extract audio using ffmpeg
+                            "key": "FFmpegExtractAudio",
+                            "preferredcodec": "m4a",
+                        }
+                    ],
                 }
             case _:
                 raise ValueError("invalid format")
 
     def get_metadata(self, url: str, fmt: Format = Format.VIDEO_AUDIO) -> MediaMetadata:
-        """ get_metadata returns MediaMetadata for the given url. """
+        """get_metadata returns MediaMetadata for the given url."""
         data = MediaMetadata(url)
         with YoutubeDL(self._format_config(fmt)) as ydl:
             info = ydl.extract_info(url, download=False)
 
             # sanitize_info required to make serializable
             response = ydl.sanitize_info(info)
-            data.name = response.get('title')
-            data.author = response.get('uploader')
-            data.author_url = response.get('uploader_url')
-            data.ts_upload = datetime.fromtimestamp(response.get('timestamp'), timezone.utc) if response.get('timestamp', False) else None
-            data.duration = response.get('duration')
+            data.name = response.get("title")
+            data.author = response.get("uploader")
+            data.author_url = response.get("uploader_url")
+            data.ts_upload = (
+                datetime.fromtimestamp(response.get("timestamp"), timezone.utc)
+                if response.get("timestamp", False)
+                else None
+            )
+            data.duration = response.get("duration")
 
-            data.thumbnail_url = response.get('thumbnail')
+            data.thumbnail_url = response.get("thumbnail")
 
-            data.format = response.get('format')
+            data.format = response.get("format")
         return data
 
-    def _get_media(self, q: queue.Queue, url: str, fmt: Format, directory: str, filename: str):
+    def _get_media(
+        self, q: queue.Queue, url: str, fmt: Format, directory: str, filename: str
+    ):
         """
         Commence a download from YouTube.
         Consider threading this to allow for asynchronous downloads.
         """
-        opts = {
-            'logger': self._Queuelogger(q),
-            'no_warnings': True,
-            'paths': {
-                'home': directory,
-                'temp': f"{directory}/temp", # currently hard-coded - should we make this configurable?
-            },
-            # Note: This will break if you were to pass in multiple target URLs
-            'outtmpl': f'{filename}.%(ext)s',
-        } | self._format_config(fmt)
+        opts = (
+            {
+                "logger": self._Queuelogger(q),
+                "no_warnings": True,
+                "paths": {
+                    "home": directory,
+                    "temp": f"{directory}/temp",  # currently hard-coded - should we make this configurable?
+                },
+                # Note: This will break if you were to pass in multiple target URLs
+                "outtmpl": f"{filename}.%(ext)s",
+            }
+            | self._format_config(fmt)
+        )
         try:
             with YoutubeDL(opts) as ydl:
                 ydl.download([url])
@@ -107,12 +119,16 @@ class YTDLPFetcher(Fetcher):
             # signals end of stream
             q.put(None)
 
-    def get_media(self, url: str, fmt: Format, directory: str, filename: str) -> Generator[str]:
-        """ get_media downloads the media at the given params in the foreground, returning log information by means of a Generator. """
+    def get_media(
+        self, url: str, fmt: Format, directory: str, filename: str
+    ) -> Generator[str]:
+        """get_media downloads the media at the given params in the foreground, returning log information by means of a Generator."""
         q = queue.Queue()
 
         # We need to run the download on a thread so we can continue to execute our client response
-        thread = threading.Thread(target=self._get_media(q, url, fmt, directory, filename), daemon=True)
+        thread = threading.Thread(
+            target=self._get_media(q, url, fmt, directory, filename), daemon=True
+        )
         thread.start()
 
         while True:
@@ -130,8 +146,8 @@ class YTDLPFetcher(Fetcher):
                     line_fmt = "color: red"
                 case "finish":
                     if payload != 0:
-                        yield f"<span style='color:red'>❌ Slurp failed!</span> Please check the logs above."
+                        yield "<span style='color:red'>❌ Slurp failed!</span> Please check the logs above."
                     else:
-                        yield f"<span style='color:green'>🥤Slurp successful</span>"
+                        yield "<span style='color:green'>🥤Slurp successful</span>"
                     continue
             yield f"<span style='{line_fmt}'>{typ}</span>: {escape(payload)}"
