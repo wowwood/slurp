ac7506af978535baec29ed230e541121c7e6567f
duck. <me@duck.me.uk>
2026-06-02 23:57:32 +0100

    Don't run test suites with huge downloads unless opted in
    Signed-off-by: duck. <me@duck.me.uk>

--- slurp/fetchers/test_ytdlp.py
+++ slurp/fetchers/test_ytdlp.py
@@ -29,6 +29,7 @@ class TestYTDLPFetcher:
         meta = fetcher_instance._get_metadata(_urls["huge"])
         assert meta is not None, "_get_metadata returned None"
 
+    @pytest.mark.network
     def test_download(self, tmp_path, fetcher_instance):
         for event in fetcher_instance.fetch(
             _urls["small"], Format.VIDEO_AUDIO, str(tmp_path.absolute()), "test"
@@ -38,10 +39,12 @@ class TestYTDLPFetcher:
             "output file does not exist (expected 'test.webm')"
         )
 
-    @pytest.mark.huge
+    @pytest.mark.network
+    @pytest.mark.huge_dl
     def test_huge_download(self, tmp_path, fetcher_instance):
         """
         Warning: this test downloads an enormous video with a huge filesize!
+        It is intended purely as a stress test.
         """
         for event in fetcher_instance.fetch(
             _urls["huge"], Format.VIDEO_AUDIO, str(tmp_path.absolute()), "test"
