a25906952c843e4f166d2de8f577f0fa1776f2f9
duck. <me@duck.me.uk>
2026-06-04 20:25:06 +0100

    Update gunicorn config & remove useless containerfiles
    Signed-off-by: duck. <me@duck.me.uk>

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -3,7 +3,7 @@
 import multiprocessing
 import os
 
-bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
+bind = f"[::]:{os.getenv('PORT', '8000')}"
 accesslog = "-"
 access_log_format = (
     "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
@@ -12,6 +12,6 @@ access_log_format = (
 workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
 threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
 
-reload = bool(os.getenv("WEB_RELOAD", "false"))
+reload = bool(os.getenv("WEB_RELOAD", False))
 
 timeout = int(os.getenv("WEB_TIMEOUT", 120))
